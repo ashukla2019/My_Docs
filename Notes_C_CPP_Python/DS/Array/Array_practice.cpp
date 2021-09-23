664beaee86d6499be521c961be489321296daa5a
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2021-09-23 17:38:38 +0530

    Add files via upload

---
+++ Notes_C_CPP_Python/DS/Array/Array_practice.cpp
@@ -0,0 +1,244 @@
+// Function to find the element  
+// occurring odd number of times 
+// XOR of all elements gives us odd occurring element
+int getOddOccurrence(int ar[], int ar_size) 
+{ 
+    int res = 0;  
+    for (int i = 0; i < ar_size; i++)      
+        res = res ^ ar[i]; 
+      
+    return res; 
+} 
+  
+// Function to get the missing number
+int getMissingNo(int a[], int n)
+{
+ 
+    int total = (n + 1) * (n + 2) / 2;
+    for (int i = 0; i < n; i++)
+        total -= a[i];
+    return total;
+}
+
+//An element is leader if it is greater than all the elements to its right side. And the rightmost element is always a leader. For ////example //int the array {16, 17, 4, 3, 5, 2}, leaders are 17, 5 and 2
+/*C++ Function to print leaders in an array */
+oid printLeaders(int arr[], int size) 
+{ 
+    int max_from_right =  arr[size-1]; 
+  
+    /* Rightmost element is always leader */
+    cout << max_from_right << " "; 
+      
+    for (int i = size-2; i >= 0; i--) 
+    { 
+        if (max_from_right <= arr[i])  
+        {            
+            max_from_right = arr[i]; 
+            cout << max_from_right << " "; 
+        } 
+    }     
+} 
+//Reverse array
+void reverseArray(int arr[], int n)
+{
+    int t, i;
+    for (i = 0; i < n/2; i++) 
+    {
+        t = arr[i];
+        arr[i] = arr[n-1-i];
+        arr[n-1-i] = t;    
+        
+    }
+}
+
+//Majority Element: A majority element in an array A[] of size n is an element that appears more than n/2 times
+void findMajority(int arr[], int size)
+{
+    unordered_map<int, int> m;
+    for(int i = 0; i < size; i++)
+        m[arr[i]]++;
+    int count = 0;
+    for(auto i : m)
+    {
+        if(i.second > size / 2)
+        {
+            count =1;
+            cout << "Majority found :- " << i.first<<endl;
+            break;
+        }
+    }
+    if(count == 0)
+        cout << "No Majority element" << endl;
+}
+
+
+//Quick sort algo
+int split(int a[], int lower, int upper)
+{
+	int pivot = a[lower];
+	int p = lower+1;
+	int q = upper;
+	int temp;
+	while(q>=p)
+	{
+		while(a[p] < pivot)
+		{
+			p++;
+		}
+		while(a[q] > pivot)
+		{
+			q--;
+		}
+		if(q>p)
+		{
+			temp = a[p];
+			a[p] = a[q];
+			a[q] = temp;
+		}
+	}
+	temp = a[lower];
+	a[lower] = a[q];
+	a[q] = temp;
+	return q;
+	
+}
+
+void quicksort(int a[], int lower, int upper)
+{
+	if(upper > lower)
+	{
+		int i = split(a, lower, upper);
+		quicksort(a, lower, i-1);
+		quicksort(a, i+1, upper);
+	}
+}
+
+//selection sort and bubble sort:
+int* selection_sort(int*p, int len)
+{
+	for(int i =0; i<len-1; i++)
+	{
+		for(int j = i+1; j<len-1; j++)
+		{
+			if(p[i]>p[j])
+			{
+				int temp = p[i];
+				p[i] = p[j];
+				p[j] = temp;
+			}
+		}
+	}
+	return p;
+}
+
+int* bubble_sort(int*p, int len)
+{
+	for(int i =0; i<len-1; i++)
+	{
+		for(int j = 0; j<=len-i-1; j++)
+		{
+			if(p[j]>p[j+1])
+			{
+				int temp = p[j];
+				p[j] = p[j+1];
+				p[j+1] = temp;
+			}
+		}
+	}
+	return p;
+}
+
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--------------------------------------------------------------------------------------
+// C function to find maximum in arr[] of size n
+int largest(int arr[], int n) {
+    int i;
+
+    // Initialize maximum element
+    int max = arr[0];
+
+    // Traverse array elements from second and
+    // compare every element with current max
+    for (i = 1; i < n; i++)
+        if (arr[i] > max) max = arr[i];
+
+    return max;
+}
+//You are given an array arr[], you have to re-construct an array arr[].
+//The values in arr[] are obtained by doing Xor of consecutive elements in the array.
+int* game_with_number(int arr[], int n)
+{
+    
+    // Complete the function
+    for(int i =0; i<n-1; i++)
+    {
+        arr[i] = arr[i]^arr[i+1];
+    }
+    return arr;
+    
+}
+//Cyclically rotate an array by one
+void rotate(int arr[], int n)
+{
+    int x = arr[n-1];
+    for(int i=n-1; i>0; i--)
+    {
+        arr[i] = arr[i-1];
+    }
+    arr[0] = x;
+}
---
+++ Notes_C_CPP_Python/DS/linked_list/list/n_from_last.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+struct node
+{
+	int data;
+	node* next;
+};
+
+void add_node(struct node** head, int data)
+{
+	node* temp = (struct node*)malloc(sizeof(node));
+	temp->data = data;
+	if(*head == nullptr)
+	{
+		*head = temp;
+		temp->next = nullptr;
+	}
+	else
+	{
+		temp->next = *head;
+		*head = temp;
+	}
+}
+
+void display(struct node* head)
+{
+	node* temp;
+	temp = head;
+	while(temp != nullptr)
+	{
+		cout<<"Values:"<<temp->data<<endl;
+		temp = temp->next;
+	}
+}
+
+int findKthNode(struct node* node, int k)
+{
+    // base case
+    if (node == NULL) {
+        return 0;
+    }
+    int count = findKthNode(node->next, k)+1;  //this function will be called 6 times in below way: stack is grwoing from below to top...
+    //<---- After finihing all calls(when it gets last node null, it returs and start executing pendinf calls for recursive calls)
+    //findKthNode(5, 4)+1  it will return count which is 0 + 1 = 1 and then will execute remaining steps(if(count == K))
+    //findKthNode(10, 4)+1 it will return count which is 1 + 1 = 2
+    //findKthNode(15, 4)+1 it will return count which is 2 + 1 = 3
+    //findKthNode(20, 4)+1 it will return count which is 3 + 1 = 4
+    //findKthNode(25, 4)+1 it will return count which is 4 + 1 = 5
+    //findKthNode(30, 4)+1 it will return count which is 5 + 1 = 6
+    if (count == k) {
+        printf("k'th node from the end is %d", node->data);
+    }
+ 
+    return count;
+}
+
+int main()
+{
+	node* head = nullptr;
+	add_node(&head, 5);
+	add_node(&head, 10);
+	add_node(&head, 15);
+	add_node(&head, 20);
+	add_node(&head, 25);
+	add_node(&head, 30);
+	//display(head);
+	findKthNode(head, 4);
+	return 0;
+	
+}
