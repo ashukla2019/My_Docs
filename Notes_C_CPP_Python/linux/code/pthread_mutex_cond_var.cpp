051754bcd13fb82d89cac50a3651fd38f75e12eb
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2021-12-06 13:58:55 +0530

    Add files via upload

--- Notes_C_CPP_Python/linux/code/pthread_mutex_cond_var.cpp
+++ Notes_C_CPP_Python/linux/code/pthread_mutex_cond_var.cpp
@@ -8,8 +8,10 @@ pthread_mutex_t mutexFuel;
 pthread_cond_t condFuel;
 int fuel = 0;
 
-void* fuel_filling(void* arg) {
-    for (int i = 0; i < 5; i++) {
+void* fuel_filling(void* arg) 
+{
+    for (int i = 0; i < 5; i++) 
+	{
         pthread_mutex_lock(&mutexFuel);
         fuel += 15;
         printf("Filled fuel... %d\n", fuel);
@@ -19,9 +21,11 @@ void* fuel_filling(void* arg) {
     }
 }
 
-void* car(void* arg) {
+void* car(void* arg) 
+{
     pthread_mutex_lock(&mutexFuel);
-    while (fuel < 40) {
+    while (fuel < 40) 
+	{
         printf("No fuel. Waiting...\n");
         pthread_cond_wait(&condFuel, &mutexFuel);
         // Equivalent to:
@@ -34,24 +38,33 @@ void* car(void* arg) {
     pthread_mutex_unlock(&mutexFuel);
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[]) 
+{
     pthread_t th[2];
     pthread_mutex_init(&mutexFuel, NULL);
     pthread_cond_init(&condFuel, NULL);
-    for (int i = 0; i < 2; i++) {
-        if (i == 1) {
-            if (pthread_create(&th[i], NULL, &fuel_filling, NULL) != 0) {
+    for (int i = 0; i < 2; i++) 
+	{
+        if (i == 1) 
+		{
+            if (pthread_create(&th[i], NULL, &fuel_filling, NULL) != 0) 
+			{
                 perror("Failed to create thread");
             }
-        } else {
-            if (pthread_create(&th[i], NULL, &car, NULL) != 0) {
+        } 
+		else 
+		{
+            if (pthread_create(&th[i], NULL, &car, NULL) != 0) 
+			{
                 perror("Failed to create thread");
             }
         }
     }
 
-    for (int i = 0; i < 2; i++) {
-        if (pthread_join(th[i], NULL) != 0) {
+    for (int i = 0; i < 2; i++) 
+	{
+        if (pthread_join(th[i], NULL) != 0) 
+		{
             perror("Failed to join thread");
         }
     }
