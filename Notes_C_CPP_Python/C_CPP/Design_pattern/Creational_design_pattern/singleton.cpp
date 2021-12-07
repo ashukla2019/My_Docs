ef07e131e92a33a2538d2c2dd0c08b67c82819f6
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2021-12-07 13:01:46 +0530

    Add files via upload

--- Notes_C_CPP_Python/C_CPP/Design_pattern/Creational_design_pattern/singleton.cpp
+++ Notes_C_CPP_Python/C_CPP/Design_pattern/Creational_design_pattern/singleton.cpp
@@ -7,12 +7,21 @@ Advantages: Save memory, Single access point(logger(error logging), database con
 Usage: Database application, Config settings(game settings, Application settings etc.)
 */
 
+/*
+Difference b/w static class and singleton:
+Main differences are:
+Singleton has an instance/object while static class is a bunch of static methods.
+Singleton can be inherited which supports open/close principles in SOLID principles on the other hand static class can't be inherited and we need to make changes in itself.
+Singleton object can be passed to methods while static class as it does not have instance can't be passed as parameters
+*/
+
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-class GameSetting{
+class GameSetting
+{
 	static GameSetting* _instance;
 	int _brightness;
 	int _width;
@@ -21,19 +30,39 @@ class GameSetting{
 	// all constructors should be private or public(iff you want to allow inheritance)
 
 public:
-	static GameSetting* getInstace() {
+	static GameSetting* getInstace() 
+	{
 		if(_instance == NULL) 
 			_instance = new GameSetting();
 		return _instance;
 	}
-	void setWidth(int width) {_width = width;}
-	void setHeight(int height) {_height = height;}
-	void setBrighness(int brightness) {_brightness = brightness;}
+	void setWidth(int width) 
+	{
+		_width = width;
+	}
+	void setHeight(int height) 
+	{
+		_height = height;
+	}
+	void setBrighness(int brightness) 
+	{
+		_brightness = brightness;
+	}
 
-	int getWidth() {return _width;}
-	int getHeight() {return _height;}
-	int getBrightness() {return _brightness;}
-	void displaySetting() {
+	int getWidth() 
+	{
+		return _width;
+	}
+	int getHeight() 
+	{
+		return _height;
+	}
+	int getBrightness() 
+	{
+		return _brightness;
+	}
+	void displaySetting() 
+	{
 		cout << "brightness: " << _brightness << endl;
 		cout << "height: " << _height << endl;
 		cout << "width: " << _width << endl << endl;
@@ -42,12 +71,14 @@ public:
 
 GameSetting * GameSetting::_instance = NULL;
 
-void someFunction () {
+void someFunction () 
+{
 	GameSetting *setting = GameSetting::getInstace();
 	setting->displaySetting();
 }
 
-int main() {
+int main() 
+{
 
 	GameSetting *setting = GameSetting::getInstace();
 	setting->displaySetting();
