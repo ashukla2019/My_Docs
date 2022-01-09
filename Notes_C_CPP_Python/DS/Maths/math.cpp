605224f5daf802d1d2edc7b19105438ad1059ab2
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2022-01-09 10:59:11 +0530

    Update math.cpp

--- Notes_C_CPP_Python/DS/Maths/math.cpp
+++ Notes_C_CPP_Python/DS/Maths/math.cpp
@@ -14,7 +14,17 @@ int MissingNumber(vector<int>& array, int n) {
         }
         return (n*(n+1)/2 - total);
     }
-    
+ 
+//Trailing zeroes in factorial :
+ int trailingZeroes(int N)
+    {
+       int count = 0;
+       
+       for(int i = 5; i <= N; i = i * 5)
+          count += N / i ;
+       
+     return count;
+    }
 
 /*Fizz Buzz:
 
