22816ca1de107aa954140678b59166a5d5fb46e7
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2022-01-09 18:56:58 +0530

    Update linked_list.cpp

--- Notes_C_CPP_Python/DS/linked_list/list/linked_list.cpp
+++ Notes_C_CPP_Python/DS/linked_list/list/linked_list.cpp
@@ -130,18 +130,30 @@ ListNode* mergeTwoLists(ListNode* list1, ListNode* list2)
 }
 
 //Detect cycle/loop in linkedlist:
-bool hasCycle(ListNode *head) 
-{
-	ListNode* fast = head;
-	ListNode* slow = head;
-	while(fast !=nullptr && fast->next != nullptr)
-	{
-		fast = fast->next->next;
-		slow = slow->next;
-		if(fast == slow)
-		{
-			return true;
-		}
-	 }
-	return false;
-}
+Linked List Cycle II
+ListNode *detectCycle(ListNode *head) {
+        if(!head){
+            return nullptr;
+        }
+        if(!head->next){
+            return nullptr;
+        }
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while(fast!=nullptr && fast->next!=nullptr)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow==fast){//detects that there is a cycle
+                slow = head;//slow assigning it to the head 
+                while(slow != fast){
+                    slow = slow->next;
+                    fast = fast->next;                    
+                }
+                return slow ;
+            }
+           
+        }
+        return nullptr;
+    }
+};
