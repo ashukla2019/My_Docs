5066142afdec3109b7feaaad5f43474e07b773eb
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2022-01-10 14:49:28 +0530

    Update string.cpp

--- Notes_C_CPP_Python/DS/String/string.cpp
+++ Notes_C_CPP_Python/DS/String/string.cpp
@@ -566,3 +566,24 @@ for(int i=0;i<S.length();i++)
     }
 }
 return str;
+
+//Recursively remove all adjacent duplicates 
+Input: s = "abbaca"
+Output: "ca"	
+string removeDuplicates(string s) 
+{
+string str = "";
+for(int i=0; i<s.length(); i++)
+{
+   if(str.size()>0 && str.back() == s[i])
+   {
+       str.pop_back();
+   }
+   else
+   {
+       str.push_back(s[i]);
+   }
+}
+return str;
+
+}	
