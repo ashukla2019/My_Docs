4e894f5b4b9d654c2690a50a8d0d6310905584a0
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2022-01-10 15:16:11 +0530

    Update string.cpp

--- Notes_C_CPP_Python/DS/String/string.cpp
+++ Notes_C_CPP_Python/DS/String/string.cpp
@@ -587,3 +587,21 @@ for(int i=0; i<s.length(); i++)
 return str;
 
 }	
+//ATOI:
+int myAtoi(string s) 
+{
+int res =0;
+int sign =1;
+int i=0;
+
+if(s[0] == '-')
+{
+    sign = -1;
+    i++;
+}
+for(; s[i] != '\0'; ++i)
+{
+    res = res*10 + s[i] - '0';
+}
+return (sign*res);
+}
