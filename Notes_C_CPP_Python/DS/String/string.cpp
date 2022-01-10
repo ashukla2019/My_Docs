9b289bdde9bbdf7818071e93c3040dff07d56c4e
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2022-01-10 12:08:41 +0530

    Update string.cpp

--- Notes_C_CPP_Python/DS/String/string.cpp
+++ Notes_C_CPP_Python/DS/String/string.cpp
@@ -509,3 +509,26 @@ string longestCommonPrefix(vector<string>& strs)
 	return ans;
 }
 };
+
+//Reverse word in given string:
+Input:
+S = i.like.this.program.very.much
+Output: much.very.program.this.like.i
+
+string reverseWords(string S) 
+{ 
+ istringstream iss(S);
+ string rtr = "";
+ string token;
+ while (std::getline(iss, token, '.')) 
+ {
+     if (!token.empty())
+     {
+	rtr = token + "." + rtr;
+
+    }
+
+}
+rtr.pop_back();
+return rtr;
+} 
