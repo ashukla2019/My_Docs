1abff0b9e69a67d67b48c9ac56d31941a82e0f6e
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2022-01-13 16:28:09 +0530

    Update bst.cpp

--- Notes_C_CPP_Python/DS/Tree/Tree/bst.cpp
+++ Notes_C_CPP_Python/DS/Tree/Tree/bst.cpp
@@ -255,7 +255,55 @@ bool isSymmetric(TreeNode* root) {
 }
 
 //Convert Sorted Array to Binary Search Tree:
+TreeNode* sortedArrayToBST(vector<int>& nums)
+{
 
+return dfs(nums, 0, nums.size()-1);
+
+}
+
+TreeNode* dfs(vector<int>& nums, int start, int end)
+{
+if(start > end)
+{
+    return nullptr;
+}
+int mid = (start + end)/2;
+TreeNode* node = new TreeNode(nums[mid]);
+node->left = dfs(nums, start, mid-1);
+node->right = dfs(nums, mid+1, end);
+return node;
+}
+
+//Function to return a list containing elements of left view of the binary tree.
+vector<int> leftView(Node *root)
+{
+if(!root) {
+    return {};
+}
+vector<int> v; //store values of nodes in the rightmost
+queue<Node*> Q; //store node type values in queue 
+Q.push(root); //push root
+while(!Q.empty()) { //repeat steps until queue is not empty
+
+    int size = Q.size();  // current size of queue
+    for(int i = 0; i < size; i++) {
+      Node* t = Q.front(); //declare a temp node and put front node of queue
+	Q.pop(); 
+	if(i==0) {   //if node is rightmost 
+	    v.push_back(t->data); //push the value of rightmost node into vector
+	}
+	if(t->left) {   //if temp->left != NULL then push into queue
+	    Q.push(t->left);
+	}
+	if(t->right) { //if temp->right != NULL then push into queue
+	    Q.push(t->right);
+	}
+    }  
+}
+return v; //finally we have all values
+    
+}
 
 int main() 
 {
@@ -305,32 +353,4 @@ int main()
 	cout<<"\n";
 }
 
-//Function to return a list containing elements of left view of the binary tree.
-vector<int> leftView(Node *root)
-{
-if(!root) {
-    return {};
-}
-vector<int> v; //store values of nodes in the rightmost
-queue<Node*> Q; //store node type values in queue 
-Q.push(root); //push root
-while(!Q.empty()) { //repeat steps until queue is not empty
 
-    int size = Q.size();  // current size of queue
-    for(int i = 0; i < size; i++) {
-      Node* t = Q.front(); //declare a temp node and put front node of queue
-	Q.pop(); 
-	if(i==0) {   //if node is rightmost 
-	    v.push_back(t->data); //push the value of rightmost node into vector
-	}
-	if(t->left) {   //if temp->left != NULL then push into queue
-	    Q.push(t->left);
-	}
-	if(t->right) { //if temp->right != NULL then push into queue
-	    Q.push(t->right);
-	}
-    }  
-}
-return v; //finally we have all values
-    
-}
