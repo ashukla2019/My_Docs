d9101c3b7a5bcc5c6b22d736fb3d5a3c2f30063e
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2022-01-14 11:17:26 +0530

    Update linked_list.cpp

--- Notes_C_CPP_Python/DS/linked_list/list/linked_list.cpp
+++ Notes_C_CPP_Python/DS/linked_list/list/linked_list.cpp
@@ -68,8 +68,6 @@ int getNthFromLast(struct node* head, int k)
     current->next = NULL;
 }
 
-
-
 //Palindrome:
 bool isPalindrome(ListNode* head) 
 {
@@ -130,30 +128,91 @@ ListNode* mergeTwoLists(ListNode* list1, ListNode* list2)
 }
 
 //Detect cycle/loop in linkedlist:
-Linked List Cycle II
-ListNode *detectCycle(ListNode *head) {
-        if(!head){
-            return nullptr;
+int detectAndRemoveLoop(struct Node* list)
+{
+    struct Node *slow_p = list, *fast_p = list;
+
+    // Iterate and find if loop exists or not
+    while (slow_p && fast_p && fast_p->next) {
+        slow_p = slow_p->next;
+        fast_p = fast_p->next->next;
+
+        /* If slow_p and fast_p meet at some point then there
+           is a loop */
+        if (slow_p == fast_p) {
+            removeLoop(slow_p, list);
+
+            /* Return 1 to indicate that loop is found */
+            return 1;
         }
-        if(!head->next){
-            return nullptr;
-        }
-        ListNode *slow = head;
-        ListNode *fast = head;
-        while(fast!=nullptr && fast->next!=nullptr)
-        {
-            slow = slow->next;
-            fast = fast->next->next;
-            if(slow==fast){//detects that there is a cycle
-                slow = head;//slow assigning it to the head 
-                while(slow != fast){
-                    slow = slow->next;
-                    fast = fast->next;                    
-                }
-                return slow ;
-            }
-           
-        }
-        return nullptr;
     }
-};
+
+    /* Return 0 to indicate that there is no loop*/
+    return 0;
+}
+
+/* Function to detect and remove loop in linkedlist
+ /* This function detects and removes loop in the list
+  If loop was there in the list then it returns 1,
+  otherwise returns 0 */
+int detectAndRemoveLoop(struct Node* list)
+{
+    struct Node *slow_p = list, *fast_p = list;
+
+    // Iterate and find if loop exists or not
+    while (slow_p && fast_p && fast_p->next) {
+        slow_p = slow_p->next;
+        fast_p = fast_p->next->next;
+
+        /* If slow_p and fast_p meet at some point then there
+           is a loop */
+        if (slow_p == fast_p) {
+            removeLoop(slow_p, list);
+
+            /* Return 1 to indicate that loop is found */
+            return 1;
+        }
+    }
+
+    /* Return 0 to indicate that there is no loop*/
+    return 0;
+}
+
+/* Function to remove loop.
+ loop_node --> Pointer to one of the loop nodes
+ head -->  Pointer to the start node of the linked list */
+void removeLoop(struct Node* loop_node, struct Node* head)
+{
+    struct Node* ptr1 = loop_node;
+    struct Node* ptr2 = loop_node;
+
+    // Count the number of nodes in loop
+    unsigned int k = 1, i;
+    while (ptr1->next != ptr2) {
+        ptr1 = ptr1->next;
+        k++;
+    }
+
+    // Fix one pointer to head
+    ptr1 = head;
+
+    // And the other pointer to k nodes after head
+    ptr2 = head;
+    for (i = 0; i < k; i++)
+        ptr2 = ptr2->next;
+
+    /*  Move both pointers at the same pace,
+      they will meet at loop starting node */
+    while (ptr2 != ptr1) {
+        ptr1 = ptr1->next;
+        ptr2 = ptr2->next;
+    }
+
+    // Get pointer to the last node
+    while (ptr2->next != ptr1)
+        ptr2 = ptr2->next;
+
+    /* Set the next node of the loop ending node
+      to fix the loop */
+    ptr2->next = NULL;
+}
