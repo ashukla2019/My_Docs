6f689f3c2644105c3b914c70c218efb9ef3bb8af
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2022-05-09 13:13:37 +0530

    Add files via upload

--- Notes_C_CPP_Python/Python/practice.py
+++ Notes_C_CPP_Python/Python/practice.py
@@ -1,14 +1,18 @@
-mylist = []
-class login:
-	def __init__(self, name):
-		self.name = name
-		mylist.append(self.name)
-	def verify(self, name):
-		for i in mylist:
-			if name == i:
-				print "welcome"+ " "+name
-			else:
-				print "you are unknown"
+class MyClass(object):
+    limit = 10
 
-log = login("Ankit")	
-log.verify("Pattu")			
+    def __init__(self):
+        self.data = []
+
+    def item(self, i):
+        return self.data[i]
+
+    def add(self, e):
+        if len(self.data) >= self.limit:
+            raise Exception("Too many elements")
+        self.data.append(e)
+
+myclass = MyClass()
+myclass.add("Amit")
+myclass.add("Ankit")
+print myclass.data
