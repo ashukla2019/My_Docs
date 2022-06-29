42d5dd57d5d14bafc24b831ddd6ffe170f46c7e8
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2022-06-29 09:46:47 +0530

    Add files via upload

---
+++ Notes_C_CPP_Python/DS/graph/adjacency_list.cpp
@@ -0,0 +1,54 @@
+// Adjascency List representation in C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Graph
+{
+	int vertex;
+	list<int>*l;
+	public:
+		Graph(int v)
+		{
+			vertex = v;
+			l = new list<int>[v];
+		}
+		void addEdge(int s, int d) 
+		{
+  			l[s].push_back(d);
+  			l[d].push_back(s);
+		}
+		void deleteEdge(int s, int d)
+		{
+			l[s].remove(d);
+			l[d].remove(s);
+		}
+		// Print the graph
+		void printGraph() 
+		{
+  			for (int d = 0; d < vertex; ++d) 
+  			{
+    			cout << "\n Vertex "<< d << ":";
+    			for (auto x : l[d])
+    			{
+      				cout << "-> " << x;
+    				printf("\n");
+  				}
+			}
+		}
+};
+
+int main() 
+{
+  int v = 5;
+  Graph g(v);
+  // Add edges
+  g.addEdge(0, 1);
+  g.addEdge(0, 2);
+  g.addEdge(0, 3);
+  g.addEdge(1, 2);
+  g.printGraph();
+  cout<<"After deleting edge\n";
+  g.deleteEdge(1, 2);
+  g.printGraph();
+}
