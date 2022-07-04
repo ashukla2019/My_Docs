55e7e99668f47acbeff11209d150076311ea34aa
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2022-07-04 16:09:23 +0530

    Add files via upload

--- Notes_C_CPP_Python/C_CPP/C++/Design_pattern/Structural_design_pattern/adapter.cpp
+++ Notes_C_CPP_Python/C_CPP/C++/Design_pattern/Structural_design_pattern/adapter.cpp
@@ -1,3 +1,9 @@
+/*
+Adapter pattern works as a bridge between two incompatible interfaces. 
+This type of design pattern comes under structural pattern as this pattern combines
+the capability of two independent interfaces.
+*/
+
 #include<iostream>
 #include<memory>
 using namespace std;
