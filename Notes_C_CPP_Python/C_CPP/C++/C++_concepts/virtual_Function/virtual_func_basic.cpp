00f6087c30ecc37ea208100e6b51d320bbfb71fb
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2022-07-08 10:58:01 +0530

    Add files via upload

--- Notes_C_CPP_Python/C_CPP/C++/C++_concepts/virtual_Function/virtual_func_basic.cpp
+++ Notes_C_CPP_Python/C_CPP/C++/C++_concepts/virtual_Function/virtual_func_basic.cpp
@@ -1,68 +1,37 @@
-#include<iosteam>
+#include <iostream>
 using namespace std;
-//Problem without Virtual function:
-class A
-{	public:
-	void fun1()
-	{
-		cout<<"A::fun1 is called"<<endl;
-	}
-};
-class B: public A
+
+class Base
 {
-	public:
-	void fun1()
-	{
-		cout<<"B::fun1 is called"<<endl;
-	}
-	void fun2()
-	{
-	cout<<"B::fun2 is called"<<endl;
-	}
+public:
+    virtual void function1() { cout<<"Base::function1\n";};
+    virtual void function2() { cout<<"Base::function2\n";};
+};
+
+class D1: public Base
+{
+public:
+    virtual void function1() { cout<<"D1::function1\n";};
+};
+
+class D2: public Base
+{
+public:
+    virtual void function2() { cout<<"D2::function2\n";};
 };
 
 int main()
 {
-	A*p, a1;
-	B b1;
-	p = &b1; //This will be executed at run time. compiler does not know if p is holding address of object of A/B.
-	b1.fun1(); //EB: compile time binding.
-	p->fun1(); //EB: compile time binding. compiler will check type of p which is of class A type. It will call class A function. Ideally we want to call function of class B, since we passed object of class B.
-	return 0;  
+    Base* b1 = new D1(); //virtual table of Derived class D1 will be called.
+    b1->function1(); //D1::vtable[0] = D1::vf1
+    b1->function2(); //Base::vtable[0] = Base::vf1(D1 class,s vtable will have 
+    return 0;
 }
-//Virtual function concept can be implemented only using pointers. Because pointer may hold address of
-//object of any class. Which is evaluated at run time. If we declare Class A's function to virtual.
-//Compiler will not do early binding. It will be then late binding.
 
-#include<iosteam>
-using namespace std;
-
-class A
-{	public:
-	virtual void fun1()
-	{
-		cout<<"A::fun1 is called"<<endl;
-	}
-};
-class B: public A
-{
-	public:
-	void fun1()
-	{
-		cout<<"B::fun1 is called"<<endl;
-	}
-	void fun2()
-	{
-	cout<<"B::fun2 is called"<<endl;
-	}
-};
-
-int main()
-{
-	A*p, a1;
-	B b1;
-	p = &b1;   //This will be executed at run time. compiler does not know if p is holding address of object of A/B.
-	b1.fun1(); //EB: compile time binding.
-	p->fun1(); //LB: Run time binding. 
-	return 0;  
-}
+/*understanding:
+If we don't declare Base class function to virtual, compiler does the early binding, despite of 
+assigning derived class object to Base class pointer. when we declare Base class function
+to virtual compiler does late binding. Now compiler will insert *_VPTR to Base class 
+and will be inherited by derived class. Compiler also creates one virtual table for each
+class. Virtual table will be called of class, whose object is passed to Base class pointer.
+*/
